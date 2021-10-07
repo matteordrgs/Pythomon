4d59fdcb1ab03bb9ec2b2911fd33a9a97dc27b3d
matteordrgs <71488186+matteordrgs@users.noreply.github.com>
2021-10-07 13:33:42 +0200

    Add files via upload

---
+++ pythomon.py
@@ -0,0 +1,17 @@
+#   Phythomon Lylian Bourgin matteo rodriges 
+#   But du jeux faire des combat de pokemon en 1 contre 1 
+#   23.09.21
+
+
+# importation du timer
+import time
+# importer la bibiotheque numpy
+import numpy as np 
+# importer le systeme numpy 
+import sys
+
+
+# LES POKEMON
+# Créer les pokemon :
+
+dracaufeu = Pokemon('dracaufeu', 'feu', ['lance flamme ', 'lance soleil', 'deflagration', 'vol'], {'ATTACK':12, 'DEFENSE': 172})
